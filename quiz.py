8c1ed0bf50f8b95e988025afd4d55120ff9ee911
Laura Webber <laura@hightouch.io>
2022-02-20 16:19:59 -0800

    added quiz pdf

---
+++ quiz.py
@@ -0,0 +1,29 @@
+berries = {
+    'berry1': 'blueberry',
+    'berry2': 'strawberry',
+    'berry3': 'raspberry'
+}
+
+fruit = {
+    'fruit1': 'apple',
+    'fruit2': 'orange',
+    'fruit3': 'banana'
+}
+
+fruit_list = [
+    {
+        'berry1': 'blueberry',
+        'berry2': 'strawberry',
+        'berry3': 'raspberry'
+    },
+    {
+        'fruit1': 'apple',
+        'fruit2': 'orange',
+        'fruit3': 'banana'
+    }
+]
+
+print(fruit_list[0]['berry1'])
+
+# How would you select 'blueberry'?
+
